278be766787e3cd4da874bd881f0c90d3214cb11
ATUL BHOJAK <atulbhojak9888@gmail.com>
2025-10-16 14:12:24 +0530

    Delete tinder-backend directory

--- tinder-backend/resources/views/emails/popular_person.blade.php
+++
@@ -1,6 +0,0 @@
-<!doctype html>
-<html><body>
-  <h2>Popular Person Alert</h2>
-  <p>{{ $person->name }} (ID: {{ $person->id }}) has {{ $person->likes_count }} likes.</p>
-  <p>Open admin: {{ config('app.url') }}/admin/people/{{ $person->id }}</p>
-</body></html>
